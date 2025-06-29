eb729fdc085f9c1aacc4e56ad239dfbd8d2a6e0a
angelica <guidolinangelica66@gmail.com>
2025-06-29 11:50:00 +0200

    INIT PROJECT

---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,13 @@
+import type {Meta, StoryObj } from "@storybook/react";
+import { Card } from "./Card"
+import { Component } from "react";
+
+const meta={
+  title : 'Components/Card',
+  component: Card,
+ parameters:{
+      layout: 'centred',
+ },
+ tags:['autodocs']
+}
+export default meta;
